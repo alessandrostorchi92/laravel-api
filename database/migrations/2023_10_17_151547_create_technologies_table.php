73ed29fdef71ad4e08bcebe016bdb613585ae826
alessandrostorchi92 <storchi.alle@gmail.com>
2023-10-17 17:27:59 +0200

    Creation Seeder TechnologiesTableSeeder

--- database/migrations/2023_10_17_151547_create_technologies_table.php
+++ database/migrations/2023_10_17_151547_create_technologies_table.php
@@ -4,24 +4,33 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
+
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+
+    public function up(): void {
         Schema::create('technologies', function (Blueprint $table) {
+
             $table->id();
+
+            $table->string('name')->nullable();
+            $table->string("colour")->nullable();
+            $table->string('icon')->nullable();
+
             $table->timestamps();
+
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+
+    public function down(): void {
         Schema::dropIfExists('technologies');
+
     }
+
 };
