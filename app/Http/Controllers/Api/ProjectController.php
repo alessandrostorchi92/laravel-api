b01aad92e96ae84852f1656679e02d20fa3c2f6d
alessandrostorchi92 <storchi.alle@gmail.com>
2023-10-18 22:58:49 +0200

    Creation key "results" to have access to $projects

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
-class ProjectController extends Controller
-{
-    //
+class ProjectController extends Controller {
+
+    public function index() {
+
+        // Recupero i dati dal DB 
+        $projects = Project::all();
+
+        // Restituisco i dati in formato JSON
+        return response()->json([
+
+            //Questo serve per poter accedere ai dati projects mediante la chiave "results"
+            "results" => $projects
+
+        ]);
+
+    }
+    
 }
