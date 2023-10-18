faf5866ef187e6ddc5411e66f25b1d57104f7bad
alessandrostorchi92 <storchi.alle@gmail.com>
2023-10-19 00:15:42 +0200

    Set up method with()->paginate(6)

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,15 +11,21 @@ class ProjectController extends Controller {
     public function index() {
 
         // Recupero i dati dal DB 
-        $projects = Project::all();
+        // $projects = Project::all(); Il metodo all() può rallentare il server per l'entità dei dati derivanti dalle chiamate API come ha fatto Islam, mandando in crush il database di yu-gi-oh
 
-        // Restituisco i dati in formato JSON
+        //Con il metodo paginate() vado a fare una request la cui risposta sarà soltanto di n elementi per pagina in  base al numero che inserisco (di default è pari a 15). In più recuperiamo in modo ottimizzato i dati dei model collegati attraverso il metodo with(). Infine, dobbiamo passare come parametri i nomi dei metodi specificati nel model oggetto al paginate()
+
+        $projects = Project::with(["type", "technologies"])->paginate(6);
+
+        // Restituisco i dati in formato JSON. Questo serve per poter accedere ai dati projects mediante la chiave "results"
         return response()->json([
 
-            //Questo serve per poter accedere ai dati projects mediante la chiave "results"
-            "results" => $projects
+            "message" => 'Projects List',
+            "results" => $projects,
 
         ]);
+
+        
 
     }
     
