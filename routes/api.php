532a277f888d2577a9a00f22917b869743355230
alessandrostorchi92 <storchi.alle@gmail.com>
2023-10-18 17:49:03 +0200

    Set up Route Api index

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Siccome siamo dentro api.php, non serve scrivere /api prima di projects perché tutte le rotte di questo file saranno raggiungibili tramite /api/pathRotta
+Route::get("projects", [ProjectController::class, 'index']);
